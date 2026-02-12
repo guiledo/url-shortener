8caf8b6b80b4f48f9516a319d26c1d64b9a3669a
Guilherme Ledo <guilhermeledochagas@gmail.com>
2026-02-12 20:10:43 -0300

    bug fixes api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,7 @@ import { shortenUrlSchema } from '../schemas/url.schema';
 const router = Router();
 const urlController = new UrlController();
 
+router.get('/', (req, res) => res.json({ message: 'URL Shortener API is running' }));
 router.get('/api/health', (req, res) => res.json({ status: 'ok' }));
 router.post('/api/shorten', validate(shortenUrlSchema), urlController.shorten.bind(urlController));
 router.get('/api/analytics/:code', urlController.getStats.bind(urlController));
